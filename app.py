3f447d3da910003c7328d1ab6244af59600b99ad
Hafiz Abdur Rehman <hafizmuhammadabdurrehman7@gmail.com>
2025-05-29 22:03:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,50 @@
 import streamlit as st
-
+# Apply custom CSS
+st.markdown("""
+    <style>
+    .stApp {
+        background: linear-gradient(135deg, #bcbcbc, #cfe2f3);
+        padding: 30px;
+        border-radius: 16px;
+        box-shadow: 0px 10px 30px rgba(0,0,0,0.3);
+    }
+    h1 {
+        text-align: center;
+        font-size: 36px;
+        color: #1e1e2f;
+    }
+    .stButton > button {
+        background: linear-gradient(45deg, #0b5394, #351c75);
+        color: white;
+        font-size: 18px;
+        padding: 10px 20px;
+        border-radius: 10px;
+        transition: 0.3s;
+        box-shadow: 0px 5px 15px rgba(0,201,255,0.4);
+    }
+    .stButton > button:hover {
+        background: linear-gradient(45deg, #92fe9d, #00c9ff);
+        transform: scale(1.05);
+        color: black;
+    }
+    .result-box {
+        font-size: 20px;
+        font-weight: bold;
+        text-align: center;
+        background: rgba(255,255,255,0.1);
+        padding: 25px;
+        margin-top: 20px;
+        border-radius: 10px;
+        box-shadow: 0px 5px 15px rgba(0,201,255,0.3);
+    }
+    .footer {
+        text-align: center;
+        margin-top: 50px;
+        font-size: 14px;
+        color: black;
+    }
+    </style>
+""", unsafe_allow_html=True)
 def convert_length(value, from_unit, to_unit):
 
     length_factors = {
